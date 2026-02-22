4feebdf9108bedc94fd22ae111fd84e9ffdb6605
dima <dhavryliuk@edu.cdv.pl>
2026-02-22 08:54:48 +0100

    rotation

--- Assets/Scripts/Fish.cs
+++ Assets/Scripts/Fish.cs
@@ -35,5 +35,9 @@ public class Fish : MonoBehaviour
             rb.linearVelocity += (Points[index] - (Vector2)transform.position).normalized * (speed * Time.deltaTime);
             
         }
+        float angle = Mathf.Atan2(rb.velocity.y, rb.velocity.x) * Mathf.Rad2Deg;
+        // Вычитаем 90, если спрайт изначально смотрит вверх, а не вправо
+        if (angle < 90 && angle > -90) transform.rotation = Quaternion.Euler(0, 180, 90 - angle);
+        else transform.rotation = Quaternion.Euler(0, 0, angle - 90f);
     }
 }
--- Assets/Scripts/PlayerMovment.cs
+++ Assets/Scripts/PlayerMovment.cs
@@ -22,12 +22,16 @@ public class PlayerMovment : MonoBehaviour
         if (Input.GetKey(KeyCode.D)) input += Vector2.right;
 
         rb.linearVelocity += input.normalized * speed * Time.deltaTime;
-        transform.rotation = Quaternion.LookRotation(rb.velocity, Vector3.up);
-        transform.up = rb.velocity.normalized;
+        /*transform.rotation = Quaternion.LookRotation(rb.velocity, Vector3.up);
+        transform.up = rb.velocity.normalized;*/
         /*if (rb.linearVelocity != Vector2.zero)
         {
             transform.Rotate(90, 0, 0);
             transform.Rotate(0, 90, 0);
         }*/
+        float angle = Mathf.Atan2(rb.velocity.y, rb.velocity.x) * Mathf.Rad2Deg;
+        // Вычитаем 90, если спрайт изначально смотрит вверх, а не вправо
+        if (angle < 90 && angle > -90) transform.rotation = Quaternion.Euler(0, 180, 90 - angle);
+        else transform.rotation = Quaternion.Euler(0, 0, angle - 90f);
     }
 }
