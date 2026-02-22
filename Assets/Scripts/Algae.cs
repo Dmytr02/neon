6d9c6965f2c8650266d177df8822d4007c49c526
dima <dhavryliuk@edu.cdv.pl>
2026-02-22 11:56:19 +0100

    fix

--- Assets/Scripts/Algae.cs
+++ Assets/Scripts/Algae.cs
@@ -3,12 +3,32 @@ using UnityEngine;
 public class Algae : MonoBehaviour, IInteractable
 {
     [SerializeField] HandAlgae HandAlgaePrefab;
+    bool isInteracting = true;
     public void Interact(Interactor interactor)
     {
+        if(!isInteracting) return;
         HandAlgae handAlgae = Instantiate(HandAlgaePrefab, transform.position, transform.rotation, interactor.transform);
         
         handAlgae.transform.localPosition = Vector3.zero;
         handAlgae.transform.localRotation = Quaternion.identity;
         Destroy(this.gameObject);
+        
+        isInteracting = false;
+        
+        Invoke("CanInteract", 30);
+        
+        foreach (var i in GetComponentsInChildren<Renderer>())
+        {
+            i.enabled = false;
+        }
+    }
+
+    void CanInteract()
+    {
+        bool isInteracting = true;
+        foreach (var i in GetComponentsInChildren<Renderer>())
+        {
+            i.enabled = true;
+        }
     }
 }
