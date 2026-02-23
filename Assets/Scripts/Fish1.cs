1f669c7830e2ace99c6fb20ba46344ed88f43d6e
dima <dhavryliuk@edu.cdv.pl>
2026-02-23 09:41:36 +0100

    last version

--- Assets/Scripts/Fish1.cs
+++ Assets/Scripts/Fish1.cs
@@ -9,19 +9,21 @@ public class Fish1 : MonoBehaviour
     
     void Update()
     {
+        Debug.Log("sadasdasd");
         rb.linearVelocity += (Vector2)(PlayerMovment.Instance.transform.position - transform.position).normalized * (speed * Time.deltaTime);
-        float angle = Mathf.Atan2(rb.linearVelocity.y, rb.linearVelocity.x) * Mathf.Rad2Deg;
+        /*float angle = Mathf.Atan2(rb.linearVelocity.y, rb.linearVelocity.x) * Mathf.Rad2Deg;
         // Вычитаем 90, если спрайт изначально смотрит вверх, а не вправо
         if (angle < 90 && angle > -90) transform.rotation = Quaternion.Euler(0, 180, 180 - angle);
-        else transform.rotation = Quaternion.Euler(0, 0, angle);
-    }
-
-    public void TryKill()
-    {
+        else transform.rotation = Quaternion.Euler(0, 0, angle);*/
         Collider2D overlap = Physics2D.OverlapCircle(transform.position, 10, 1 << LayerMask.NameToLayer("Player"));
         if (overlap)
         {
             Debug.Log("Death");
         }
+    }
+
+    public void TryKill()
+    {
+        
     }
 }
